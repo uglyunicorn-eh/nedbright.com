170e81a4bba044bb93efe4cafcb189b7e52578de
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-15 15:13:57 -0800

    Add @astrojs/cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
-import keystatic from '@keystatic/astro'
+
+import keystatic from '@keystatic/astro';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    markdoc(),
-    keystatic(),
-  ],
+  integrations: [tailwind(), react(), markdoc(), keystatic()],
   output: 'hybrid',
+  adapter: cloudflare()
 });
