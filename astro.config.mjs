1d033889580c0bb9e2be52d46fde7afe9811b7d4
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-15 12:05:07 -0800

    Add React and Markdoc integrations, update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
+    react(),
+    markdoc(),
   ],
 });
