e4bf6190c1eadaacf3276a9f6407d9aadc800297
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-17 11:50:55 -0800

    Update file paths and import statements

--- src/reader.ts
+++ src/reader.ts
@@ -1,5 +1,5 @@
 import { createReader } from '@keystatic/core/reader';
 
-import keystaticConfig from '../keystatic.config';
+import keystaticConfig from 'keystatic.config';
 
 export const reader = createReader(process.cwd(), keystaticConfig);
