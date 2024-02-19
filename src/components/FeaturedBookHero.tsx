958e2a0acceda65ee0f68522c4ce47d2adae06b9
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-19 11:29:54 -0800

    Refactor image styling in FeaturedBookHero component

--- src/components/FeaturedBookHero.tsx
+++ src/components/FeaturedBookHero.tsx
@@ -20,7 +20,7 @@ export const FeaturedBookHero = ({ book }: ComponentProps) => {
           </div>
 
           <div className="relative z-10 mx-auto flex w-64 rounded-xl shadow-2xl shadow-black/15 md:w-80 lg:w-96 aspect-[3/4]">
-            <img className="w-full rounded-xl" src={book.data.image} alt={book.data.title} />
+            <img className="w-full object-cover rounded-xl" src={book.data.image} alt={book.data.title} />
           </div>
 
         </div>
