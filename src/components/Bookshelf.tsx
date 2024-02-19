e25a4152ce80c4bf3b79ed8fc198d0e20580ef3b
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-19 01:04:16 -0800

    Update Bookshelf and Layout styles

--- src/components/Bookshelf.tsx
+++ src/components/Bookshelf.tsx
@@ -40,7 +40,7 @@ export const Bookshelf = ({ books }: ComponentProps) => {
         <div className="flex justify-center">
           <a
             href={`/books`}
-            className="rounded-full ring-indigo-600/60 hover:ring-indigo-600 shadow-indigo-600 ring-2 px-6 py-2.5 text-indigo-600"
+            className="rounded-full ring-indigo-600/60 hover:ring-indigo-600 shadow-indigo-600 hover:bg-indigo-600/5 ring-2 px-6 py-2.5 text-indigo-600"
           >
             Все книги и материалы &rarr;
           </a>
