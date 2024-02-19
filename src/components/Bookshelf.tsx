c7c70eef140b541a035c0c2b9181792d0255f8d0
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-19 00:25:01 -0800

    Update bookshelf description

--- src/components/Bookshelf.tsx
+++ src/components/Bookshelf.tsx
@@ -16,7 +16,7 @@ export const Bookshelf = ({ books }: ComponentProps) => {
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Библиотека</h2>
         <p className="mt-2 text-gray-600 text-balance">
-          Здесь вы найдете книги, рабочие тетради, другие материалы для самостоятельного изучения и практики.
+          Здесь вы найдете книги, рабочие тетради и другие материалы для самостоятельного изучения и практики.
         </p>
       </div>
 
