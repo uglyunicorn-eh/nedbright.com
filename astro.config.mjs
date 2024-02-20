0eb6e97b4da6befc393db96a3583aa1ac87b13a9
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-20 01:48:36 -0800

    Add @astrojs/sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
+import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 
 import keystatic from '@keystatic/astro';
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), markdoc(), keystatic()],
+  integrations: [tailwind(), react(), markdoc(), keystatic(), sitemap()],
   output: 'hybrid',
   adapter: cloudflare(),
   site: import.meta.env.SITE_URL,
