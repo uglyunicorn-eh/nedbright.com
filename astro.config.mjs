dd5be6bf0f518b6d1ffb10986f35129f0f55c637
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-21 19:35:03 -0800

    Update robots.txt policy to allow all user agents to access the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
     sitemap(),
     robotsTxt({
       policy: [
-        { userAgent: "*", disallow: "/" },
+        { userAgent: "*", allow: "/" },
       ],
     }),
   ],
