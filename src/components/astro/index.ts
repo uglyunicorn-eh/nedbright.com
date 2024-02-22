64fa57873ed40f2056e0ead22c98ae51a4d64ef4
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-21 18:27:21 -0800

    Refactor components to be astro

---
+++ src/components/astro/index.ts
@@ -0,0 +1,5 @@
+export { default as FeaturedBookHero } from './FeaturedBookHero.astro';
+export { default as Bookshelf } from './Bookshelf.astro';
+export { default as BooksList } from './BooksList.astro';
+export { default as BookDetailsHead } from './BookDetailsHead.astro';
+export { default as Testimonial } from './Testimonial.astro';
