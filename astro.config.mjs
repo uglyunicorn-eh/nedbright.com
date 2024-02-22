92e6df4216e2340de32b34f03880b1e7e6a9b212
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-21 20:37:13 -0800

    Add maskable icon and update webmanifest configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,15 @@ export default defineConfig({
   }), webmanifest({
     name: "Жить среди людей",
     icon: "src/assets/favicon.svg",
+    icons: [{
+      "src": "src/assets/maskable_icon.png",
+      "sizes": "1024x1024",
+      "type": "image/png",
+      "purpose": "maskable"
+    }],
     theme_color: "#cfbcdc",
+    background_color: "#cfbcdc",
+    display: "standalone",
   })],
   output: 'hybrid',
   adapter: cloudflare(),
