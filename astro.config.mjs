cceffdc3adac54fea1c268bc2d69d62ab52b0826
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-21 20:24:23 -0800

    Add astro-webmanifest integration to package.json and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,23 @@ import markdoc from "@astrojs/markdoc";
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import keystatic from '@keystatic/astro';
-
 import robotsTxt from "astro-robots-txt";
+
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    markdoc(),
-    keystatic(),
-    sitemap(),
-    robotsTxt({
-      policy: [
-        { userAgent: "*", allow: "/" },
-      ],
-    }),
-  ],
+  integrations: [tailwind(), react(), markdoc(), keystatic(), sitemap(), robotsTxt({
+    policy: [{
+      userAgent: "*",
+      allow: "/"
+    }]
+  }), webmanifest({
+    name: "Жить среди людей",
+    icon: "src/assets/favicon.svg",
+    theme_color: "#cfbcdc",
+  })],
   output: 'hybrid',
   adapter: cloudflare(),
-  site: "https://ng.nedbright.com"
+  site: "https://ng.nedbright.com",
 });
