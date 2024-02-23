9fb52f2be3393dc0eeffef795eb08467d25b4b11
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-22 21:52:40 -0800

    Refactor config.ts to define collections for books and writings

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,23 +1,31 @@
 import { defineCollection, z } from 'astro:content';
 
-const booksCollection = defineCollection({
-  type: 'content',
-  schema: ({ image }) =>
-    z.object({
-      image: image(),
-      title: z.string().optional(),
-      isFeatured: z.boolean().optional(),
-      headline: z.string().optional(),
-      link: z.string().optional(),
-      testimonies: z.array(z.object({
-        quote: z.string().optional(),
-        author: z.string().optional(),
-        showAuthor: z.boolean().optional(),
-        isFeatured: z.boolean().optional(),
-      })).optional(),
-    }),
-});
-
 export const collections = {
-  'books': booksCollection,
+  'books': defineCollection({
+    type: 'content',
+    schema: ({ image }) =>
+      z.object({
+        image: image(),
+        title: z.string().optional(),
+        isFeatured: z.boolean().optional(),
+        headline: z.string().optional(),
+        link: z.string().optional(),
+        testimonies: z.array(z.object({
+          quote: z.string().optional(),
+          author: z.string().optional(),
+          showAuthor: z.boolean().optional(),
+          isFeatured: z.boolean().optional(),
+        })).optional(),
+      }),
+  }),
+
+  'writings': defineCollection({
+    type: 'content',
+    schema: ({ image }) =>
+      z.object({
+        title: z.string().optional(),
+        headline: z.string().optional(),
+        date: z.string().optional(),
+      }),
+  }),
 };
