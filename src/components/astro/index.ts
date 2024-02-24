2c8e59ed698f40164d59fe72662c89a27769c0b6
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-24 09:00:55 -0800

    Writings page

--- src/components/astro/index.ts
+++ src/components/astro/index.ts
@@ -6,6 +6,8 @@ export { default as Testimonial } from './Testimonial.astro';
 export { default as Footer } from './Footer.astro';
 export { default as WritingsHero } from './WritingsHero.astro';
 export { default as FeaturedWritingPreview } from './FeaturedWritingPreview.astro';
+export { default as FeaturedWritingPreviewHD } from './FeaturedWritingPreviewHD.astro';
 export { default as TextWritingPreview } from './TextWritingPreview.astro';
+export { default as TextWritingPreviewHD } from './TextWritingPreviewHD.astro';
 export { default as Date } from './Date.astro';
 export { default as WritingMetaDetails } from './WritingMetaDetails.astro';
