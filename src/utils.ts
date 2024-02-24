1cf6d4354058ae7f62a1fbaa731a7fa0592bdb7c
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-02-23 16:29:58 -0800

    Add readingTime function to calculate estimated reading time

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,7 @@
 export const sample = <T>(items: Array<T>) => items ? items[Math.floor(Math.random() * items.length)] : undefined;
+
+export const readingTime = (text: string, wpm?: number = 225) => {
+  const words = text.trim().split(/\s+/).length;
+  const time = Math.ceil(words / wpm);
+  return time;
+}
