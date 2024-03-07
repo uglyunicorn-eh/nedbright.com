72daf6cc0136b423ac49e0eb30b1496ab763072a
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-03-06 22:17:41 -0800

    Add email validation and error handling to POST endpoint

--- src/pages/api/knock-knock.ts
+++ src/pages/api/knock-knock.ts
@@ -1,6 +1,31 @@
+import { z, ZodError } from "zod";
+
+import type { APIContext } from 'astro';
+
 export const prerender = false;
 
 export async function GET() {
   const number = Math.random();
   return Response.json({ number, message: `Here's a random number: ${number}` });
+}
+
+const Input = z.object({
+  email: z.string().email(),
+});
+
+export async function POST({ request }: APIContext) {
+  try {
+    const { email } = Input.parse(await request.json());
+
+    return Response.json({
+      status: 'ok',
+      email,
+    });
+  }
+  catch (error) {
+    if (error instanceof ZodError) {
+      return Response.json({ status: 'error', errors: error.errors }, { status: 400 });
+    }
+    return Response.json({ status: 'error' }, { status: 400 });
+  }
 }
