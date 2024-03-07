a0655491688695c15ecdd346cdbb5263c9a1e0cf
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-03-06 21:14:59 -0800

    Commented out random number generation in GET function

--- src/pages/api/knock-knock.ts
+++ src/pages/api/knock-knock.ts
@@ -1,6 +1,6 @@
 export const prerender = false;
 
 export async function GET() {
-  const number = Math.random();
-  return Response.json({ number, message: `Here's a random number: ${number}` });
+  // const number = Math.random();
+  // return Response.json({ number, message: `Here's a random number: ${number}` });
 }
