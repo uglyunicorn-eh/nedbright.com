30bf77ba6737310c198c4cf9995f107fa9101e9f
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-03-06 21:55:16 -0800

    Remove unused TypeScript files

--- functions/knock-knock.ts
+++
@@ -1,9 +0,0 @@
-import { PagesFunction, Response } from "@cloudflare/workers-types";
-
-interface Env {
-  // KV: KVNamespace;
-}
-export const onRequest: PagesFunction<Env> = async (context) => {
-  // const value = await context.env.KV.get('example');
-  return Response.json({ message: "Who's there?" });
-}
