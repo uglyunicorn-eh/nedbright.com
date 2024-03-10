86889534e14338809524746fcc4a7fcd396fffd9
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-03-10 16:57:41 -0700

    Add ProfilePanel component and update profile page

--- src/components/react/index.ts
+++ src/components/react/index.ts
@@ -3,3 +3,5 @@ export { GridPattern } from './GridPattern';
 export { StarRating } from './StarRating';
 export { SocialIcon } from './SocialIcon';
 export { ReplyBox } from './ReplyBox';
+export { Navbar } from './Navbar';
+export { ProfilePanel } from './ProfilePanel';
