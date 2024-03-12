172b83e306083a5d96e1c6f9de8d20d43b87d0e3
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-03-12 12:36:24 -0700

    Add useProfile hook for reading profile badge cookie

---
+++ src/components/react/useProfile.ts
@@ -0,0 +1,37 @@
+import * as jose from "jose";
+import Cookies from 'js-cookie';
+import React from "react";
+
+const PUBLIC_KEY = await jose.importSPKI(import.meta.env.PUBLIC_KEY.replaceAll('\\n', '\n'), "RS256");
+
+export type Profile = {
+  iat: number;
+  name?: string;
+  'replybox:sso'?: {
+    hash: string;
+    payload: string;
+  },
+}
+
+const readProfileBadge = async () => {
+  const profileBadgeCookie = Cookies.get('X-Profile-Badge');
+  if (!profileBadgeCookie) {
+    return undefined;
+  }
+  const knockKnockToken = await jose.jwtVerify(profileBadgeCookie, PUBLIC_KEY);
+  return knockKnockToken.payload as Profile;
+}
+
+export const useProfile = () => {
+  const [profileBadge, setProfileBadge] = React.useState<Profile>();
+
+  React.useEffect(() => {
+    readProfileBadge().then(setProfileBadge);
+  },
+    [],
+  );
+
+  return ({
+    profileBadge,
+  });
+};
