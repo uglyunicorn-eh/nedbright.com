c011fce97c8f70d8fddef064663eb9fc9833c955
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-03-26 00:43:51 +0800

    Add Navbar component

---
+++ src/components/react/Navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <div className=" p-4 md:p-6 my-nav-active">
+      <div className="container mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quas iure! Dolorum quam non voluptates vero totam quaerat nobis quis recusandae culpa consectetur, laudantium, saepe, vitae assumenda quidem qui magnam!
+      </div>
+    </div>
+  )
+}
