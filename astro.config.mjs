aba58ca3582fee9e40cd22db591e8da563ebae7e
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-07-22 21:15:12 -0700

    chore: Comment out Sentry configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import robotsTxt from "astro-robots-txt";
 import keystatic from '@keystatic/astro';
 import webmanifest from "astro-webmanifest";
 
-import sentry from "@sentry/astro";
+// import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -51,17 +51,17 @@ export default defineConfig({
       display: "standalone"
     }),
 
-    sentry({
-      dsn: import.meta.env.PUBLIC_SENTRY_DSN,
-      environment: import.meta.env.PROD ? "production" : "development",
-      enabled: import.meta.env.PROD,
-      attachStacktrace: true,
-      sourceMapsUploadOptions: {
-        project: "nedbright-com",
-        authToken: import.meta.env.SENTRY_AUTH_TOKEN,
-      },
-      enableTracing: true,
-    }),
+    // sentry({
+    //   dsn: import.meta.env.PUBLIC_SENTRY_DSN,
+    //   environment: import.meta.env.PROD ? "production" : "development",
+    //   enabled: import.meta.env.PROD,
+    //   attachStacktrace: true,
+    //   sourceMapsUploadOptions: {
+    //     project: "nedbright-com",
+    //     authToken: import.meta.env.SENTRY_AUTH_TOKEN,
+    //   },
+    //   enableTracing: true,
+    // }),
   ],
   output: 'hybrid',
   adapter: cloudflare({
