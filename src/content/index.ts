6ae12e1f9bb683408195501fda6aa2c7708754ae
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-07-24 22:58:07 -0700

    Fix collection

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,13 +1,1 @@
-import { type Entry } from '@keystatic/core/reader';
-import keystaticConfig from 'keystatic.config';
-
-import { reader } from "src/reader";
-
-export type Collections = {
-  books: Entry<typeof keystaticConfig['collections']['books']>,
-  writings: Entry<typeof keystaticConfig['collections']['writings']>,
-};
-
-export const getCollection = async <K extends keyof Collections>(collectionName: K) => {
-  return await reader.collections[collectionName].all();
-}
+export type { Book, Testimony, Writing } from './config';
