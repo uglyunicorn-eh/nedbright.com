839123ecab2b158b9485e65da7fb63a8baa0751d
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-07-28 21:18:52 -0700

    refactor: Remove console.log statements in useProfile.ts

--- src/components/react/useProfile.ts
+++ src/components/react/useProfile.ts
@@ -16,7 +16,9 @@ const readProfileBadge = async () => {
   if (!profileBadgeCookie) {
     return undefined;
   }
+  console.log({ profileBadgeCookie });
   const knockKnockToken = await jose.decodeJwt(profileBadgeCookie);
+  console.log({ knockKnockToken });
   return knockKnockToken.payload as Profile;
 }
 
