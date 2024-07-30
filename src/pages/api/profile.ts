cbb900c83f084ac068d8272862747f662651fcaf
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-07-30 00:14:54 -0700

    refactor: Update profile API endpoint to read idToken from request headers

--- src/pages/api/profile.ts
+++ src/pages/api/profile.ts
@@ -2,9 +2,30 @@ import type { APIContext } from "astro";
 
 export const prerender = false;
 
-export async function GET({ locals, cookies }: APIContext) {
+export async function GET({ locals, request }: APIContext) {
+  const cookieString = request.headers.get("Cookie")
+  const idToken = getCookie(cookieString, "X-Identity-Badge");
   return Response.json({
     status: 'ok',
-    cookies,
+    idToken,
   });
+}
+
+/**
+ * Takes a cookie string
+ * @param {String} cookieString - The cookie string value: "val=key; val2=key2; val3=key3;"
+ * @param {String} key - The name of the cookie we are reading from the cookie string
+ * @returns {(String|null)} Returns the value of the cookie OR null if nothing was found.
+ */
+function getCookie(cookieString, key) {
+  if (cookieString) {
+    const allCookies = cookieString.split("; ")
+    const targetCookie = allCookies.find(cookie => cookie.includes(key))
+    if (targetCookie) {
+      const [_, value] = targetCookie.split("=");
+      return (value || "").trim() || null;
+    }
+  }
+
+  return null;
 }
