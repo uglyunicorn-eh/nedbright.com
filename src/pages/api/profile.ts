b250a6bf3c17a6d532d71431a622f0ed9c89baf8
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-07-30 09:45:45 -0700

    refactor: Update profile API endpoint to verify idToken using public key

--- src/pages/api/profile.ts
+++ src/pages/api/profile.ts
@@ -1,15 +1,33 @@
+import * as jose from "jose";
 import type { APIContext } from "astro";
 
 export const prerender = false;
 
-export async function GET({ request }: APIContext) {
+export async function GET({ request, locals }: APIContext) {
+  const {
+    DOMAIN,
+  } = locals.runtime.env;
+
   const cookieString = request.headers.get("Cookie");
-  const idToken = getCookie(cookieString || "", "X-Identity-Badge");
-  if (!idToken) {
+  const idTokenValue = getCookie(cookieString || "", "X-Identity-Badge");
+  if (!idTokenValue) {
     return Response.json({ status: 'error', message: 'Unauthorized' }, { status: 401 });
   }
+
+  const PUBLIC_KEY = await jose.importSPKI(locals.runtime.env.PUBLIC_KEY.replaceAll('\\n', '\n'), "RS256");
+  const idToken = await jose.jwtVerify(
+    idTokenValue,
+    PUBLIC_KEY,
+    {
+      audience: DOMAIN,
+      issuer: DOMAIN,
+      typ: "JWT",
+    },
+  );
+
   return Response.json({
     status: 'ok',
+    idToken,
   });
 }
 
