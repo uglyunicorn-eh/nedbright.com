dd370f99c629032834d94d2a84583033af094ea6
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-07-30 00:21:46 -0700

    refactor: Add authorization check to profile API endpoint

--- src/pages/api/profile.ts
+++ src/pages/api/profile.ts
@@ -2,8 +2,11 @@ import type { APIContext } from "astro";
 
 export const prerender = false;
 
-export async function GET({ locals, request }: APIContext) {
-  const cookieString = request.headers.get("Cookie")
+export async function GET({ request }: APIContext) {
+  const cookieString = request.headers.get("Cookie");
+  if (!cookieString) {
+    return Response.json({ status: 'error', message: 'Unauthorized' }, { status: 401 });
+  }
   const idToken = getCookie(cookieString, "X-Identity-Badge");
   return Response.json({
     status: 'ok',
@@ -11,13 +14,7 @@ export async function GET({ locals, request }: APIContext) {
   });
 }
 
-/**
- * Takes a cookie string
- * @param {String} cookieString - The cookie string value: "val=key; val2=key2; val3=key3;"
- * @param {String} key - The name of the cookie we are reading from the cookie string
- * @returns {(String|null)} Returns the value of the cookie OR null if nothing was found.
- */
-function getCookie(cookieString, key) {
+function getCookie(cookieString: string, key: string) {
   if (cookieString) {
     const allCookies = cookieString.split("; ")
     const targetCookie = allCookies.find(cookie => cookie.includes(key))
