9306612cd9b6bc217d4dd7df1e1e5cd3f5d60778
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-08-17 10:38:09 -0700

    rewrite sign in using zodiac

--- src/pages/api/knock-knock.ts
+++ src/pages/api/knock-knock.ts
@@ -1,22 +1,20 @@
 import { Buffer } from 'node:buffer';
 import * as jose from "jose";
-import { z, ZodError } from "zod";
-import type { APIContext } from 'astro';
+import { z } from "zod";
 import { createHmac } from 'node:crypto';
 
-import { issueIdentityBadge } from 'src/utils/auth';
 import { zodiac } from 'src/utils/zodiac';
 
 export const prerender = false;
 
-const Query = z.object({
-  token: z.string(),
-});
+const _ = z.object;
 
-const KnockKnock = z.object({
+const KnockKnockTokenSchema = _({
   cty: z.literal("X-Knock-Knock"),
   sub: z.string().email(),
 });
+
+type KnockKnockToken = z.infer<typeof KnockKnockTokenSchema>;
 
 type User = {
   iat: number;
@@ -28,118 +26,87 @@ type User = {
   },
 };
 
-export async function GET({ request, locals, cookies, redirect, url }: APIContext) {
-  const {
-    DOMAIN,
-  } = locals.runtime.env;
-  const SITE_URL = `${url.protocol}//${url.host}`;
 
-  const contentType = request.headers.get('Content-Type');
-  const isJsonResponse = contentType === 'application/json';
+const SignInSchema = {
+  input: _({
+    token: z.string(),
+  }).strict(),
+};
 
-  try {
-    const PRIVATE_KEY = await jose.importPKCS8(locals.runtime.env.PRIVATE_KEY.replaceAll('\\n', '\n'), "RS256");
-    const PUBLIC_KEY = await jose.importSPKI(locals.runtime.env.PUBLIC_KEY.replaceAll('\\n', '\n'), "RS256");
-    const { token } = Query.parse({ token: new URL(request.url).searchParams.get('token') });
+export const GET = zodiac()
+  .input(SignInSchema)
+  .use('auth')
+  .use('tokens')
+  .use('site')
+  .handle(
+    async ctx => {
+      const { input: { token }, locals, jwtVerify, issueIdentityBadge, setCookie, makeUrl, redirect } = ctx;
+      const {
+        Users,
+      } = locals.runtime.env;
 
-    const knockKnockToken = await jose.jwtVerify(
-      token,
-      PUBLIC_KEY,
-      {
-        audience: DOMAIN,
-        issuer: DOMAIN,
-        typ: "JWT",
-      },
-    );
+      try {
+        const knockKnockToken = await jwtVerify<KnockKnockToken>(token);
 
-    const knockKnockRequest = KnockKnock.parse({
-      cty: knockKnockToken.protectedHeader.cty,
-      sub: knockKnockToken.payload.sub,
-    });
+        const at = Date.now();
 
-    const iat = Math.floor(Date.now() / 1000);
-    const sub = knockKnockRequest.sub;
+        const iat = Math.floor(at / 1000);
+        const sub = knockKnockToken.payload.sub;
 
-    const { Users } = locals.runtime.env;
+        let user: User | null = await Users.get(sub, { type: 'json' });
 
-    let user: User | null = await Users.get(sub, { type: 'json' });
+        if (!user) {
+          user = { iat, sub, name: undefined, 'replybox:sso': undefined };
+          await Users.put(sub, JSON.stringify(user));
+        }
 
-    if (!user) {
-      user = { iat, sub, name: undefined, 'replybox:sso': undefined };
+        const maxAge = 60 * 60 * 24 * 30;
+        const expires = new Date(at + maxAge * 1000);
 
-      await Users.put(sub, JSON.stringify(user));
-    }
+        setCookie('X-Identity-Badge', await issueIdentityBadge({ sub, iat }), expires, maxAge);
 
-    const identity = await issueIdentityBadge({ sub, iat }, { locals });
-
-    const payload = Buffer.from(JSON.stringify({
-      user: {
-        name: "Pavel Reznikov",
-        email: "pashka.reznikov@gmail.com",
-      },
-      login_url: `${SITE_URL}/profile/`,
-    })).toString('base64');
-
-    const profile = (
-      await new jose
-        .SignJWT({
-          iss: DOMAIN,
-          aud: DOMAIN,
-          iat,
-          name: user.name ?? "Pavel Reznikov",
-          "replybox:sso": user['replybox:sso'] ?? {
-            hash: createHmac('sha256', locals.runtime.env.REPLYBOX_SECRET_KEY).update(payload).digest('hex'),
-            payload,
+        const payload = Buffer.from(JSON.stringify({
+          user: {
+            name: "Pavel Reznikov",
+            email: "pashka.reznikov@gmail.com",
           },
-        })
-        .setProtectedHeader({
-          alg: "RS256",
-          typ: "JWT",
-          cty: "X-Profile-Badge",
-        })
-        .sign(PRIVATE_KEY)
-    );
+          login_url: makeUrl('/profile/'),
+        })).toString('base64');
 
-    const maxAge = 60 * 60 * 24 * 30;
-    const expires = new Date(Date.now() + maxAge * 1000);
+        const {
+          DOMAIN,
+        } = locals.runtime.env;
+        const PRIVATE_KEY = await jose.importPKCS8(locals.runtime.env.PRIVATE_KEY.replaceAll('\\n', '\n'), "RS256");
+        const profile = (
+          await new jose
+            .SignJWT({
+              iss: DOMAIN,
+              aud: DOMAIN,
+              iat,
+              name: user.name ?? "Pavel Reznikov",
+              "replybox:sso": user['replybox:sso'] ?? {
+                hash: createHmac('sha256', locals.runtime.env.REPLYBOX_SECRET_KEY).update(payload).digest('hex'),
+                payload,
+              },
+            })
+            .setProtectedHeader({
+              alg: "RS256",
+              typ: "JWT",
+              cty: "X-Profile-Badge",
+            })
+            .sign(PRIVATE_KEY)
+        );
 
-    cookies.set(
-      'X-Identity-Badge',
-      identity,
-      import.meta.env.PROD
-        ? { httpOnly: true, secure: true, sameSite: 'strict', domain: DOMAIN, maxAge, path: '/', expires }
-        : { httpOnly: true, maxAge, path: '/', expires },
-    );
+        setCookie('X-Profile-Badge', profile, expires, maxAge);
 
-    if (isJsonResponse)
-      return Response.json({ status: 'ok', profile });
-
-    cookies.set(
-      'X-Profile-Badge',
-      profile,
-      import.meta.env.PROD
-        ? { httpOnly: false, secure: true, sameSite: 'strict', domain: DOMAIN, maxAge, path: '/', expires }
-        : { httpOnly: false, maxAge, path: '/', expires },
-    );
-
-    return redirect(user.name ? "/" : "/profile/", 307);
-  }
-  catch (error) {
-    console.error(error);
-
-    if (!isJsonResponse) {
-      return Response.redirect(`${SITE_URL}/401/`, 307);
+        return redirect(user.name ? "/" : "/profile/", 307);
+      }
+      catch {
+        return redirect('/401/', 307);
+      }
     }
+  );
 
-    if (error instanceof ZodError) {
-      return Response.json({ status: 'error', errors: error.errors }, { status: 400 });
-    }
-
-    return Response.json({ status: '0xdeadbeef' }, { status: 400 });
-  }
-}
-
-const _ = z.object;
 
 const SignInRequestSchema = {
   input: _({
@@ -151,11 +118,12 @@ export const POST = zodiac()
   .input(SignInRequestSchema)
   .use('auth')
   .use('sendgrid')
+  .use('site')
   .handle(
     async ctx => {
-      const { input: { email }, issueSignInToken, sendgridSend, link: mkLink, ok } = ctx;
+      const { input: { email }, issueSignInToken, sendgridSend, makeUrl, ok } = ctx;
 
-      const link = mkLink('/api/knock-knock/', { token: await issueSignInToken({ sub: email }) });
+      const link = makeUrl('/api/knock-knock/', { token: await issueSignInToken({ sub: email }) });
       await sendgridSend(email, "SIGN_IN", { link });
 
       return ok();
