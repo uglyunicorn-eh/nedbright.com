c630a8419efb0a5dff8c27fe8f52085d9a5efeef
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-08-16 22:50:19 -0700

    Use utilities instead of direct implementation

--- src/pages/api/knock-knock.ts
+++ src/pages/api/knock-knock.ts
@@ -3,6 +3,8 @@ import * as jose from "jose";
 import { z, ZodError } from "zod";
 import type { APIContext } from 'astro';
 import { createHmac } from 'node:crypto';
+import { issueIdentityBadge, issueSignInToken } from 'src/utils/auth';
+import { sendgridSend } from 'src/utils/sendgrid';
 
 export const prerender = false;
 
@@ -13,10 +15,6 @@ const Query = z.object({
 const KnockKnock = z.object({
   cty: z.literal("X-Knock-Knock"),
   sub: z.string().email(),
-});
-
-const Input = z.object({
-  email: z.string().email(),
 });
 
 type User = {
@@ -71,22 +69,7 @@ export async function GET({ request, locals, cookies, redirect, url }: APIContex
       await Users.put(sub, JSON.stringify(user));
     }
 
-    const identity = (
-      await new jose
-        .SignJWT({
-          iss: DOMAIN,
-          aud: DOMAIN,
-          iat,
-          exp: iat + 60 * 60 * 24 * 30,
-          sub: user.sub,
-        })
-        .setProtectedHeader({
-          alg: "RS256",
-          typ: "JWT",
-          cty: "X-Identity-Badge",
-        })
-        .sign(PRIVATE_KEY)
-    );
+    const identity = await issueIdentityBadge({ sub, iat }, { locals });
 
     const payload = Buffer.from(JSON.stringify({
       user: {
@@ -155,60 +138,29 @@ export async function GET({ request, locals, cookies, redirect, url }: APIContex
   }
 }
 
-export async function POST({ request, locals, url }: APIContext) {
+export async function POST(ctx: APIContext) {
+  const Input = z.object({
+    email: z.string().email(),
+  }).strict();
+
+  const { request, locals, url } = ctx;
   const {
-    DOMAIN,
-    SENDGRID_API_KEY,
-    SENDGRID_FROM_EMAIL,
-    SENDGRID_FROM_NAME,
-    SENDGRID_REPLY_TO,
     SIGN_IN_TEMPLATE_ID,
   } = locals.runtime.env;
-  const SITE_URL = `${url.protocol}//${url.host}`;
 
   try {
-    const PRIVATE_KEY = await jose.importPKCS8(locals.runtime.env.PRIVATE_KEY.replaceAll('\\n', '\n'), "RS256");
-
     const { email } = Input.parse(await request.json());
-    const iat = Math.floor(Date.now() / 1000);
 
-    const token = (
-      await new jose
-        .SignJWT({
-          iss: DOMAIN,
-          aud: DOMAIN,
-          iat,
-          exp: iat + 600 * 20,
-          sub: email,
-        })
-        .setProtectedHeader({
-          alg: "RS256",
-          typ: "JWT",
-          cty: "X-Knock-Knock",
-        })
-        .sign(PRIVATE_KEY)
-    );
+    const token = await issueSignInToken({ sub: email }, ctx);
 
-    await fetch("https://api.sendgrid.com/v3/mail/send", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${SENDGRID_API_KEY}`,
-        "Content-Type": "application/json",
+    await sendgridSend(
+      SIGN_IN_TEMPLATE_ID,
+      email,
+      {
+        link: `${url.protocol}//${url.host}/api/knock-knock/?token=${token}`,
       },
-      body: JSON.stringify({
-        personalizations: [
-          {
-            to: [{ email }],
-            dynamic_template_data: {
-              link: `${SITE_URL}/api/knock-knock/?token=${token}`,
-            },
-          },
-        ],
-        from: { email: SENDGRID_FROM_EMAIL, name: SENDGRID_FROM_NAME },
-        reply_to: { email: SENDGRID_REPLY_TO },
-        template_id: SIGN_IN_TEMPLATE_ID,
-      }),
-    });
+      ctx,
+    );
 
     return Response.json({ status: 'ok' });
   }
