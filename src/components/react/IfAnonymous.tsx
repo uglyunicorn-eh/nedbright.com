1a0d9b61ec08c0cedbdba30ad8e360d4a03c5075
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-08-23 14:27:09 -0700

    Refactor IfAnonymous component and remove console.log

--- src/components/react/IfAnonymous.tsx
+++ src/components/react/IfAnonymous.tsx
@@ -2,7 +2,6 @@ import { useProfile } from "src/components/react/useProfile";
 
 export const IfAnonymous = ({ children }: { children: React.ReactNode }) => {
   const { profileBadge } = useProfile();
-  console.log({ profileBadge });
   if (profileBadge) {
     return null;
   }
